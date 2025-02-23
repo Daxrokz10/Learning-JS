bd1a76260beab98def4fd3108281c286fcdc93cf
Daxrokz10 <dakshgagnani@gmail.com>
2025-02-23 09:28:21 +0530

    bs js

--- Color_Flipper/script.js
+++ Color_Flipper/script.js
@@ -1,14 +1,31 @@
-const body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName('body')[0]
 
 function changeColor(name){
     body.style.backgroundColor = name;
 }
 
-function randomColor(){
+
+function randomColor() {
     const red = Math.round(Math.random() * 255);
     const green = Math.round(Math.random() * 255);
     const blue = Math.round(Math.random() * 255);
-    
+
     const color = `rgb(${red}, ${green}, ${blue})`;
     body.style.backgroundColor = color;
 }
+
+
+// const body = document.getElementsByTagName("body")[0];
+
+// function changeColor(name){
+//     body.style.backgroundColor = name;
+// }
+
+// function randomColor(){
+//     const red = Math.round(Math.random() * 255);
+//     const green = Math.round(Math.random() * 255);
+//     const blue = Math.round(Math.random() * 255);
+    
+//     const color = `rgb(${red}, ${green}, ${blue})`;
+//     body.style.backgroundColor = color;
+// }
